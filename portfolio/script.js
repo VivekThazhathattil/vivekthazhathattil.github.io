4cc12ad2d1a255b5708acfb872e0c3c15880e29f
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2021-06-29 15:05:51 +0530

    add interests, blog, skills

--- portfolio/script.js
+++ portfolio/script.js
@@ -107,6 +107,8 @@ $(document).ready( ()=>{
 		if ($(window).width() < 850){
 			disp = "column";	
 			rot = "";
+			h = "auto";
+			w = "auto";
 		}
 
 		console.log($(window).width());
@@ -119,7 +121,7 @@ $(document).ready( ()=>{
 				"width" : w,
 				"display" : "flex",
 				"flex-direction" : disp,
-				"justify-content" : "flex-start",
+				"justify-content" : "space-between",
 				"background-color" : pastelColors[i],
 				"z-index" : "5",
 			})
@@ -132,18 +134,39 @@ $(document).ready( ()=>{
 		$("#exit-options-" + i.toString()).append('<button id="close-button-' + i.toString() + '" class="transparent-button"><img src="img/close.svg" class="contact-icon"/></button>');
 	}
 
-	$("#sub-content-0").append('<p class="big-font"> Hi! I\'m <b class="name-emphasis">Vivek!</b> I\'m an aerospace engineer with a passion for programming. I spend a lot of time solving problems that I think I could never solve and then take joy in proving me wrong. I\'m a very curious guy, and I\'m always open to learn anything new that I can apply it to my advantage. All the success I had in my life comes from strong willpower and determination, which makes me a firm believer of the proverb: <br><em>Where there\'s a will, there\'s a way!</em></p>');
+	$("#sub-content-0").append('<p class="big-font"> Hi! I\'m <b class="name-emphasis">Vivek!</b> I\'m an aerospace engineer with a passion for programming. I spend most of my time solving problems that I think I can never solve and then take joy in proving me wrong. I\'m a very curious guy, and I\'m always open to learning anything new that I can use it to my advantage. All the success I had in my life comes from strong willpower and determination, which makes me a firm believer in the proverb: <br><em>Where there\'s a will, there\'s a way!</em></p>');
 
-
-
-	$("#sub-content-1").append('<h2 id="progLangHeader">Programming Languages</h2>');
+	$("#sub-content-1").append('<h2 id="progLangHeader">Programming Languages:</h2>');
 	$("#sub-content-1").append('<div id="progLangContainer"> </div>');
+	$("#progLangContainer").append('<img class="skill0 good-size" src="img/skills/c.svg" />');
 	$("#progLangContainer").append('<img class="skill1 good-size" src="img/skills/cpp.svg" />');
 	$("#progLangContainer").append('<img class="skill2 good-size" src="img/skills/python.svg" />');
 	$("#progLangContainer").append('<img class="skill3 good-size" src="img/skills/java.svg" />');
 	$("#progLangContainer").append('<img class="skill4 good-size" src="img/skills/js.svg" />');
-	$("#sub-content-1").append('<img class="skill5 good-size" src="img/skills/bash.svg" />');
+	$("#progLangContainer").append('<img class="skill5 good-size" src="img/skills/bash.svg" />');
+	$("#progLangContainer").append('<img class="skill6 good-size" src="img/skills/matlab.svg" />');
+	$("#progLangContainer").append('<img class="skill7 good-size" src="img/skills/html.svg" />');
+	$("#progLangContainer").append('<img class="skill8 good-size" src="img/skills/css.svg" />');
 
+	$("#sub-content-1").append('<h2 id="progToolHeader">Tools:</h2>');
+	$("#sub-content-1").append('<div id="progToolContainer"> </div>');
+	$("#progToolContainer").append('<img class="skill0 good-size" src="img/skills/jquery.svg" />');
+	$("#progToolContainer").append('<img class="skill1 good-size" src="img/skills/scss.svg" />');
+	$("#progToolContainer").append('<img class="skill2 good-size" src="img/skills/git.svg" />');
+	$("#progToolContainer").append('<img class="skill3 good-size" src="img/skills/godot.svg" />');
+	$("#progToolContainer").append('<img class="skill4 good-size" src="img/skills/react.svg" />');
+	$("#progToolContainer").append('<img class="skill5 good-size" src="img/skills/android_studio.svg" />');
+
+	$("#sub-content-3").append('<div id="hobby-main-container" class=""></div>');
+	$("#hobby-main-container").append('<div id="hobby-container1" class="card"></div>');
+	$("#hobby-container1").append('<h2 id="chessHeader">Chess</h2>');
+	$("#hobby-container1").append('<img class="nice-size" src="img/skills/chess.svg" />');
+
+	$("#hobby-main-container").append('<div id="hobby-container2" class="card"></div>');
+	$("#hobby-container2").append('<h2 id="musicHeader">Music</h2>');
+	$("#hobby-container2").append('<img class="nice-size" src="img/skills/music.svg" />');
+
+	$("#sub-content-4").append('<h1 class="white-text huge-font">COMING SOON!</h1>');
 	/* escape button event */
   $(document).on('keyup', function(event) {
     if(event.key == "Escape") {
